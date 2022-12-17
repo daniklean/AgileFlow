3cc3ce59fd035474f6c83025ab8cc619b9dee6f7
daniklean <danielvieras10@gmail.com>
2022-12-16 23:22:44 -0400

    feat: :sparkles: Environment variable configuration features
    Added new function, config environment variables, global prefix, CORS implementation.
    
    FT: Luis Daniel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morgan('dev'));
+  const configService = app.get(ConfigService);
+  app.enableCors(CORS);
+  app.setGlobalPrefix('api/v1');
+  await app.listen(configService.get('PORT'));
+  console.log(`Server Application Up: ${await app.getUrl()}`);
 }
 bootstrap();
--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 
-@Controller('api/v1/users')
+@Controller('users')
 export class UsersController {
   constructor(private readonly UsersService: UsersService) {}
 
