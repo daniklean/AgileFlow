c6e62800a91181d5083d2f14087379a64ef0619b
daniklean <danielvieras10@gmail.com>
2023-07-15 11:37:25 -0400

    feat: :seedling: Add feature seed for typeorm on object DataSourceConfig to connect db
    Add feature seed for typeorm on object DataSourceConfig to connect db
    
    Breaking Changes: Add feature seed for typeorm on object DataSourceConfig to connect db

---
+++ src/config/data.source.ts
@@ -0,0 +1,26 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const configService = new ConfigService();
+
+ConfigModule.forRoot({
+  envFilePath: `.${process.env.NODE_ENV}.env`,
+});
+
+export const DataSourceConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const DataSourceApp: DataSource = new DataSource(DataSourceConfig);
