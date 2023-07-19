52a3eb0e2d2bf9de30c6be92429aff8045bb75a4
daniklean <danielvieras10@gmail.com>
2023-07-19 16:55:48 -0400

    feat: :necktie: Add services managements on users module
    Add services managements on users module and import Injection pattern repository with TypeORM
    
    Breaking Changes: Add services managements user module

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,8 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+
+import { UsersEntity } from '../entities/users.entity';
+import { UserDTO, UserUpdateDTO } from '../dto/users.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World users mens!';
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly userRepository: Repository<UsersEntity>,
+  ) {}
+
+  public async createUser(body: UserDTO): Promise<UsersEntity | null> {
+    try {
+      return await this.userRepository.save(body);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async findUsers(): Promise<UsersEntity[] | null> {
+    try {
+      return await this.userRepository.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async findUserByID(id: string): Promise<UsersEntity | null> {
+    try {
+      return await this.userRepository
+        .createQueryBuilder('user')
+        .where({ id })
+        .getOne();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async updateUser(
+    id: string,
+    body: UserUpdateDTO,
+  ): Promise<UpdateResult | null> {
+    try {
+      const user: UpdateResult = await this.userRepository.update(id, body);
+      if (user.affected === 0) {
+        return null;
+      }
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async deleteUser(id: string): Promise<DeleteResult | null> {
+    try {
+      const user: DeleteResult = await this.userRepository.delete(id);
+      if (user.affected === 0) {
+        return null;
+      }
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
+import { UsersEntity } from './entities/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
   providers: [UsersService],
   controllers: [UsersController],
 })
