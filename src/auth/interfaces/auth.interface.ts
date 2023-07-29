5f2ee73a29532e1e3230f2d4ccbbc0c41bfd5798
daniklean <danielvieras10@gmail.com>
2023-07-28 20:57:09 -0400

    feat(New Architecture): :sparkles: Add feature interfaces for Auth
     Interfaces added to use for Auth Module
    
     Breaking Changes: Interfaces implement to Auth Module

--- src/auth/interfaces/auth.interface.ts
+++ src/auth/interfaces/auth.interface.ts
@@ -17,3 +17,16 @@ export interface IAuthLogin {
   username: string;
   password: string;
 }
+
+export interface IAuthTokenResult {
+  role: string;
+  sub: string;
+  iat: number;
+  exp: number;
+}
+
+export interface IUseToken {
+  role: string;
+  sub: string;
+  isExpired: boolean;
+}
