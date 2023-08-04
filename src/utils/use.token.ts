57f02f22fa06903c56e5398237bab841fa81b841
daniklean <danielvieras10@gmail.com>
2023-08-03 22:57:53 -0400

    refactor(New Architecture): :sparkles: Remove hardcored sub and implement keys decorators.
    Remove hardcored sub and implement keys decorators.
    
    Breakin Changes: Remove hardcored sub and implement keys decorators.

--- src/utils/use.token.ts
+++ src/utils/use.token.ts
@@ -9,8 +9,7 @@ export const useToken = (token: string): IUseToken | string => {
     const expiresDate = new Date(decode.exp);
 
     return {
-      //sub: decode.sub,
-      sub: "fff67f9f-b569-48c6-a445-9ad31f7acee8",
+      sub: decode.sub,
       role: decode.role,
       isExpired: Number(expiresDate) <= Number(currentDate) / 1000,
       
