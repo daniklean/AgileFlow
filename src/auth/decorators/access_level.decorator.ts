6a9312f47ca270fc96b3bc56bdc40ce19e80c9db
daniklean <danielvieras10@gmail.com>
2023-08-08 20:30:32 -0400

    refactor(Module's): :passport_control: Refactor to decorator metadata
     Refactor decorator AccessLevel to type keyof typeof ROLE ACCESSLEVEL

--- src/auth/decorators/access_level.decorator.ts
+++ src/auth/decorators/access_level.decorator.ts
@@ -1,6 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 
 import { ACCESS_LEVEL_KEY } from '../../config/keys.decorators';
+import { ACCESS_LEVEL } from '../../config/roles';
 
-export const AccessLevel = (level: number) =>
+export const AccessLevel = (level: keyof typeof ACCESS_LEVEL) =>
   SetMetadata(ACCESS_LEVEL_KEY, level);
