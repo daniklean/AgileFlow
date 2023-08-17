8db1d3f66c112958f56959477fed4f5cca36eee4
daniklean <danielvieras10@gmail.com>
2023-08-16 20:30:30 -0400

    refactor(Module's): :sparkles: Add new imports and providers in Product Module
    Add UsersProjectsEntity and User Service
    
    Breaking Changes: Add entity and service from users module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsController } from './controllers/projects.controller';
 import { ProjectsService } from './services/projects.service';
 import { ProjectEntity } from './entities/projects.entity';
+import { UsersProjectsEntity } from '../users/entities/usersProjects.entity';
+import { UsersService } from '../users/services/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  imports: [TypeOrmModule.forFeature([ProjectEntity, UsersProjectsEntity])],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
+  providers: [ProjectsService, UsersService],
 })
 export class ProjectsModule {}
