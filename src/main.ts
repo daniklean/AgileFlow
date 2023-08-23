0e85f86f62baa30d02971543f4a819228eaa4570
daniklean <danielvieras10@gmail.com>
2023-08-22 20:30:30 -0400

    refactor(Module's): :sparkles: Add config swagger in main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import * as morgan from 'morgan';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { CORS } from './config';
@@ -29,7 +30,16 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(configService.get('PORT'));
+  const options = new DocumentBuilder()
+    .setTitle('AgileFlow API')
+    .setDescription('The API Manage Projects: Flowing Agilely Through Tasks.')
+    .setVersion('1.0')
+    // .addTag('Tasks, Projects, Kanban')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('AgileFlow/docs', app, document);
+
+   await app.listen(configService.get('PORT'));
   console.log(`Server Application Up: ${await app.getUrl()}`);
 }
 bootstrap();
